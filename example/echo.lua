8234631c770a7b845a2b828291d83c95625ca295
Masatoshi Teruya <mah0x211@gmail.com>
2016-03-18 19:49:02 +0900

    change: example/echo.lua has been updated.

--- example/echo.lua
+++ example/echo.lua
@@ -1,5 +1,5 @@
 --[[
-  
+
   Copyright (C) 2015 Masatoshi Teruya
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -24,176 +24,112 @@
   lua-coevent
 
   Created by Masatoshi Teruya on 15/10/06.
-  
+
 --]]
 
-local signal = require('signal');
-local bind = require('llsocket').inet.bind;
-local close = require('llsocket').close;
-local listen = require('llsocket').listen;
-local accept = require('llsocket').accept;
-local recv = require('llsocket').recv;
-local send = require('llsocket').send;
-local CoEvent = require('coevent');
+local coevent = require('coevent');
+local InetServer = require('net.stream.inet.server');
+local yield = coroutine.yield;
 -- constants
-local SOCK_STREAM = require('llsocket').SOCK_STREAM;
-local EV_READABLE = CoEvent.EV_READABLE;
-local EV_WRITABLE = CoEvent.EV_WRITABLE;
-local EV_SIGNAL = CoEvent.EV_SIGNAL;
 local HOST = '127.0.0.1';
 local PORT = '5000';
-local NONBLOCK = true;
 
 
-local function pushSendQ( req, msg )
-    req.sendqTail = req.sendqTail + 1;
-    req.sendq[req.sendqTail] = msg;
-end
+local function handleClient( sock )
+    local evRead, msg, again, err, ishup;
 
+    -- print('handleClient', sock:fd());
+    -- close socket
+    coevent.deferCo(function( sock )
+        -- print('defer handleClient', sock:fd() );
+        sock:close();
+    end, sock );
 
-local function writefd( req, msg )
-    local len, err, again = send( req.fd, msg );
-
-    -- no space of send buffer
-    if again then
-        pushSendQ( req, msg );
-        assert( req.handler:watchWritable( req.fd ) );
-    -- got error
-    elseif err then
-        close( req.fd );
-    -- close by peer
-    elseif not len then
-        close( req.fd );
+    evRead, err = coevent.watchReadable( sock:fd() );
+    if err then
+        error( err );
     end
-end
 
-
-local function onClientEvent( req, ev, evtype, hup )
-    if hup then
-        close( req.fd );
-    -- recv a message on readable event
-    elseif evtype == EV_READABLE then
-        local msg, err, again = recv( req.fd );
-
-        if not again then
-            -- got error
-            if err then
-                close( req.fd );
-            -- close by peer
-            elseif not msg then
-                close( req.fd );
-            -- reply
-            elseif req.sendqTail == 0 then
-                writefd( req, msg );
-            -- push sendq
-            else
-                pushSendQ( req, msg );
-            end
-        end
-    -- consume a sendq on writable event
-    elseif evtype == EV_WRITABLE then
-        local sendq, head, tail = req.sendq, req.sendqHead, req.sendqTail;
-        local len, err, again;
-
-        for i = head, tail do
-            len, err, again = send( req.fd, sendq[i] );
-            if again then
+    while true do
+        msg, err, again = sock:recv();
+        -- print('recv', msg, err, again)
+        -- got internal error
+        if err then
+            error( err );
+        elseif again then
+            -- print('yield recv')
+            _, _, ishup = yield();
+            -- print('resume recv', ishup)
+            -- closed by peer
+            if ishup then
                 break;
-            elseif err then
-                print( err );
-                close( req.fd );
-                return;
-            -- close by peer
-            elseif not len then
-                close( req.fd );
-                return;
             end
-
-            -- remove message
-            sendq[i] = nil;
-            head = i;
-        end
-
-        -- reset sendq and head/tail position
-        if head == tail then
-            req.sendqHead, req.sendqTail = 1, 0;
-            -- unwatch writable event
-            ev:unwatch();
+        -- closed by peer
+        elseif not msg then
+            -- print('no msg');
+            break;
+        else
+            len, err, again = sock:send( msg );
+            -- print('send', len, err, again)
+            if err then
+                print(err);
+                break;
+            end
         end
     end
-end
-
-
-local function onAccept( server, ev, evtype, hup )
-    if hup then
-        close( server.fd );
-    else
-        local fd = assert( accept( server.fd ) );
-        local c = {
-            fd = fd,
-            sendq = {},
-            sendqHead = 1,
-            sendqTail = 0
-        };
-
-        -- create client fd handler
-        c.handler = assert( server.co:createHandler( onClientEvent, c ) );
-        -- watch readable event
-        assert( c.handler:watchReadable( fd ) );
-    end
-end
-
-
-local function onSignal( server )
-    -- stop event loop
-    server.running = nil;
 end
 
 
 local function createServer()
     -- create loop
-    local server = {
-        running = true
-    };
-    local sigh, h, fd, err;
+    local server = InetServer.new({
+        host = HOST;
+        port = PORT,
+        reuseaddr = true,
+        nonblock = true
+    });
+    local evRead, ishup, sock, again, err;
 
-    -- create coevent
-    server.co = assert( CoEvent.default );
-    -- create bind socket
-    server.fd = assert( bind( HOST, PORT, SOCK_STREAM, NONBLOCK, true ) );
-    err = listen( server.fd );
+    err = err or server:listen( BACKLOG );
     if err then
         error( err );
     end
 
-    -- create server fd handler
-    h = assert( server.co:createHandler( onAccept, server ) );
-    -- watch readable event
-    assert( h:watchReadable( server.fd ) );
+    evRead, err = coevent.watchReadable( server:fd() );
+    if err then
+        error( err );
+    end
 
-    -- block SIGINT
-    signal.block( signal.SIGINT );
-    -- create signal handler
-    sigh = assert( server.co:createHandler( onSignal, server ) );
-    -- watch SIGINT
-    assert( sigh:watchSignal( signal.SIGINT ) );
-
-    -- run
+    -- start server
     print( 'start server: ', HOST, PORT );
-    repeat
-        local handler, ev, evtype, ishup, err = server.co:getevent();
 
+    while true do
+        sock, err, again = server:accept();
+        -- print('accept', sock, err, again)
+        -- got error
         if err then
             print( err );
-            break;
-        elseif handler then
-            handler:invoke( ev, evtype, ishup );
+        -- got client
+        elseif sock then
+            err = coevent.spawn( handleClient, sock );
+            -- print('spawn', err);
+        -- wait event
         else
-            break;
+            -- print('yield accept');
+            _, _, ishup = yield();
+            -- print('resume accept', ishup);
+            -- event hangup
+            if ishup then
+                -- print('close server');
+                server:close();
+                break;
+            end
         end
-    until not server.running;
+    end
+
     print( 'end server' );
 end
 
-createServer();
+coevent.runloop( createServer );
+
 
